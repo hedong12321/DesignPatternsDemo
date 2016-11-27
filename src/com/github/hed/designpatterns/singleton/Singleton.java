318ae25d38ddd4495a936fcd0be9e377760f0674
dong.he <comeonhd@sina.com>
2016-11-27 19:18:29 +0800

    测试远程仓库是否关联成功

--- src/com/github/hed/designpatterns/singleton/Singleton.java
+++ src/com/github/hed/designpatterns/singleton/Singleton.java
@@ -9,7 +9,7 @@ package com.github.hed.designpatterns.singleton;
  */
 public class Singleton {
     private static final Singleton singleton = new Singleton();
-    // 限制产生多个实例（私有构造方法）
+    // 限制产生多个实例（私有构造方法，单例的关键）
     private Singleton(){}
     // 通过该方法获取实例对象
     public static Singleton getSingleton() {
