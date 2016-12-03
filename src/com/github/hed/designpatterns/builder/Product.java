f4b43658fa32cb4ed969f3a31094152f6744d301
dong.he <comeonhd@sina.com>
2016-12-03 11:58:41 +0800

    建造者模式

---
+++ src/com/github/hed/designpatterns/builder/Product.java
@@ -0,0 +1,12 @@
+﻿package com.github.hed.designpatterns.builder;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public class Product {
+	
+	public void doSomething(){
+		//独立业务处理
+	}
+}
