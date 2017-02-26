848762d4a3d17ddedd14753b7df14044e868a05a
dong.he <comeonhd@sina.com>
2017-02-26 10:59:32 +0800

    策略、适配器、迭代模式

---
+++ src/com/github/hed/designpatterns/strategy/common/ConcreteStrategy1.java
@@ -0,0 +1,13 @@
+﻿package com.github.hed.designpatterns.strategy.common;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public class ConcreteStrategy1 implements Strategy {
+
+	public void doSomething() {
+		System.out.println("具体策略1的运算法则");
+	}
+
+}
---
+++ src/com/github/hed/designpatterns/strategy/common/Strategy.java
@@ -0,0 +1,11 @@
+﻿package com.github.hed.designpatterns.strategy.common;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public interface Strategy {
+	
+	//策略模式的运算法则
+	public void doSomething();
+}
