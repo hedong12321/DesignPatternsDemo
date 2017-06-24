4b20863b23f0b449ef5d796b4545e51aac79ff4c
dong.he <comeonhd@sina.com>
2017-06-24 20:59:11 +0800

    访问者模式

---
+++ src/com/github/hed/designpatterns/visitor/Element.java
@@ -0,0 +1,13 @@
+﻿package com.github.hed.designpatterns.visitor;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public abstract class Element {
+	//定义业务逻辑
+	public abstract void doSomething();
+	
+	//允许谁来访问
+	public abstract void accept(IVisitor visitor);
+}
